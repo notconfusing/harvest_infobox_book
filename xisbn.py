542017def3f2686ea7b758d2381faefbbc5a6161
notconfusing <isalix@gmail.com>
2013-10-09 18:41:44 -0700

    tokenize

--- xisbn.py
+++ xisbn.py
@@ -1,12 +1,32 @@
 import urllib2
 import re
 import ast
+import md5
+import time
 
 class isbnError(Exception):
     def __init__(self, value):
         self.value = value
     def __str__(self):
         return repr(self.value)
+
+def myExternalIP(counter=0):
+    url = "http://checkip.dyndns.org"
+    request = urllib2.urlopen(url).read()
+    theIP = re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3}", request)
+    return theIP[0]
+
+externalIP =''
+for i in range(100):
+    try:     
+        externalIP = myExternalIP()
+        break
+    except:
+        if i < 99:
+            continue
+        else:
+            raise isbnError('could not get external ip')
+        
 
 def xisbn(search_isbn, metadata=False):
     non_decimal = re.compile(r'[^\dxX]+')
@@ -19,7 +39,11 @@ def xisbn(search_isbn, metadata=False):
         method = '?method=getMetadata'
         fl = '&fl=*'
     rformat = '&format=python'
-    url = pre + decimal_search_isbn + method + rformat + fl
+    token = '&token=246'
+    m = md5.new(pre + decimal_search_isbn +"|"+externalIP+"|"+'tourniquet')
+    hash = '&hash='+ m.hexdigest()
+    url = pre + decimal_search_isbn + method + rformat + fl + token + hash
+    print url
     try:
         data = urllib2.urlopen(url)
     except:
@@ -60,5 +84,6 @@ def xisbn(search_isbn, metadata=False):
         
 if __name__ == '__main__':
     search_isbn = '978-1-59253-447-0'
+    print externalIP
     print xisbn(search_isbn)
     print xisbn(search_isbn, metadata=True)
